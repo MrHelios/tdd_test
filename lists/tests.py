3ca340d6b86d167fff9e7b525333c12127bcaff8
lucho <lhitaliani@gmail.com>
2016-03-08 02:00:39 -0300

    Creacion de List y su test

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from .views import home_page
-from .models import Item
+from .models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -18,25 +18,20 @@ class HomePageTest(TestCase):
         html_esperado = render_to_string('home.html', request=request)
         self.assertEqual(response.content.decode(), html_esperado)
 
-    def test_home_pagina_display_toda_lista(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('item 1', response.content.decode())
-        self.assertIn('item 2', response.content.decode())
-
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_guardar_retornar_items(self):
+        list_ = List()
+        list_.save()
+
         primer_item = Item()
         primer_item.text = 'El primer Item.'
+        primer_item.list = list_
         primer_item.save()
 
         segundo_item = Item()
         segundo_item.text = 'El segundo Item.'
+        segundo_item.list = list_
         segundo_item.save()
 
         items_guardados = Item.objects.all()
@@ -45,7 +40,9 @@ class ItemModelTest(TestCase):
         primer_item_guardado = items_guardados[0]
         segundo_item_guardado = items_guardados[1]
         self.assertEqual(primer_item_guardado.text, 'El primer Item.')
+        self.assertEqual(primer_item_guardado.list, list_)
         self.assertEqual(segundo_item_guardado.text, 'El segundo Item.')
+        self.assertEqual(segundo_item_guardado.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -54,41 +51,25 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='item 1')
-        Item.objects.create(text='item 2')
-
-        response = self.client.get('/lists/la-Lista/')
-
-        self.assertContains(response, 'item 1')
-        self.assertContains(response, 'item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='item 1', list=list_)
+        Item.objects.create(text='item 2', list=list_)        
 
 class NewLiveTest(TestCase):
 
-    def test_home_page_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'Un nuevo item'
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        nuevo_item = Item.objects.first()
-        self.assertEqual(nuevo_item.text, 'Un nuevo item')
-
-        html_esperado = render_to_string(
-                'home.html',
-                {'new_item_text': 'Un nuevo item'}, request=request)
-
-    def test_home_pagina_solo_guardado_items(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = 'Una nueva lista item'
-
-        response = home_page(request)
-
+    def test_guardar_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'Un nuevo item'}
+        )
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'Una nueva lista item')
+        self.assertEqual(new_item.text, 'Un nuevo item')
 
+    def test_redirect_desp_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'Un nuevo item'}
+        )
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/la-Lista/')
